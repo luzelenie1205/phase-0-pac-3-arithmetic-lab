f700b0579af3fdcc7ec01a80fa8da0a6fb10db4b
luzelenie <luzelenie1205@gmail.com>
2021-07-12 02:13:27 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+function add(a,b){
+return a+b
+}
+function subtract(a,b){
+return a-b
+}
+function multiply(a,b){
+return a*b
+}
+function divide(a,b){
+return a/b
+}
+
+function increment(n){
+    return n + 1
+}
+
+function decrement(n){
+    return n - 1
+}
+function makeInt(n){
+    const New= parseInt(n,10);
+    return New;
+   }
+
+   function preserveDecimal(n){
+    const New = parseFloat(n);
+    return New
+   }
